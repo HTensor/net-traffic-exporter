75762760609465b8c88de13a86af6ddb1a79e145
Lei Shi <me@leishi.io>
2020-10-15 20:58:49 -0700

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -10,47 +10,47 @@ REMOTE_PORT=$4
 set_forward () {
     if [ -z "$($SUDO cat /etc/sysctl.conf | grep 'net.ipv4.ip_forward = 1')" ]; then
         echo "net.ipv4.ip_forward = 1" | $SUDO tee -a /etc/sysctl.conf
-        $SUDO sysctl -p 
+        $SUDO sysctl -p
     fi
 }
 
 forward () {
     set_forward
     $SUDO iptables -t nat -A PREROUTING -p tcp --dport $LOCAL_PORT -j DNAT --to-destination $REMOTE_IP:$REMOTE_PORT  -m comment --comment "FORWARD $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
+    $SUDO iptables -t nat -A PREROUTING -p udp --dport $LOCAL_PORT -j DNAT --to-destination $REMOTE_IP:$REMOTE_PORT  -m comment --comment "FORWARD $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
     $SUDO iptables -t nat -A POSTROUTING -d $REMOTE_IP -p tcp --dport $REMOTE_PORT -j SNAT --to-source $INET -m comment --comment "BACKWARD $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
+    $SUDO iptables -t nat -A POSTROUTING -d $REMOTE_IP -p udp --dport $REMOTE_PORT -j SNAT --to-source $INET -m comment --comment "BACKWARD $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
     $SUDO iptables -A FORWARD -p tcp -d $REMOTE_IP --dport $REMOTE_PORT -j ACCEPT -m comment --comment "UPLOAD $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
+    $SUDO iptables -A FORWARD -p udp -d $REMOTE_IP --dport $REMOTE_PORT -j ACCEPT -m comment --comment "UPLOAD-UDP $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
     $SUDO iptables -A FORWARD -p tcp -s $REMOTE_IP -j ACCEPT -m comment --comment "DOWNLOAD $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
+    $SUDO iptables -A FORWARD -p udp -s $REMOTE_IP -j ACCEPT -m comment --comment "DOWNLOAD-UDP $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
     $SUDO iptables-save
 }
 
 monitor () {
     set_forward
     $SUDO iptables -A INPUT -p tcp -d $INET --dport $LOCAL_PORT -m comment --comment "UPLOAD $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
+    $SUDO iptables -A INPUT -p udp -d $INET --dport $LOCAL_PORT -m comment --comment "UPLOAD-UDP $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
     $SUDO iptables -A INPUT -p tcp -s $REMOTE_IP -d $INET -m comment --comment "DOWNLOAD $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
+    $SUDO iptables -A INPUT -p udp -s $REMOTE_IP -d $INET -m comment --comment "DOWNLOAD-UDP $LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
     $SUDO iptables-save
 }
 
+list () {
+    COMMENT="$LOCAL_PORT->"
+    $SUDO iptables -S | grep $COMMENT
+    $SUDO iptables -t nat -S | grep $COMMENT
+}
+
 delete () {
-    COMMENT="$LOCAL_PORT->$REMOTE_IP:$REMOTE_PORT"
-    while [[ ! -z "$($SUDO iptables -L INPUT --line-numbers | grep $COMMENT)" ]]
+    COMMENT="$LOCAL_PORT->"
+    while [[ ! -z "$($SUDO iptables -S | grep $COMMENT)" ]]
     do
-        $SUDO iptables -L INPUT --line-numbers | grep $COMMENT | awk '{print $1}' | xargs $SUDO iptables -D INPUT 
+    	$SUDO iptables -S | grep $COMMENT | awk -v SUDO="$SUDO" '{$1="";$COMMEND=SUDO" iptables -D "$0; system($COMMEND)}'
     done
-    while [[ ! -z "$($SUDO iptables -L OUTPUT --line-numbers | grep $COMMENT)" ]]
+    while [[ ! -z "$($SUDO iptables -t nat -S | grep $COMMENT)" ]]
     do
-        $SUDO iptables -L OUTPUT --line-numbers | grep $COMMENT | awk '{print $1}' | xargs $SUDO iptables -D OUTPUT 
-    done
-    while [[ ! -z "$($SUDO iptables -L FORWARD --line-numbers | grep $COMMENT)" ]]
-    do
-        $SUDO iptables -L FORWARD --line-numbers | grep $COMMENT | awk '{print $1}' | xargs $SUDO iptables -D FORWARD 
-    done
-    while [[ ! -z "$($SUDO iptables -t nat -L PREROUTING --line-numbers | grep $COMMENT)" ]]
-    do
-        $SUDO iptables -t nat -L PREROUTING --line-numbers | grep $COMMENT | awk '{print $1}' | xargs $SUDO iptables -t nat -D PREROUTING 
-    done
-    while [[ ! -z "$($SUDO iptables -t nat -L POSTROUTING --line-numbers | grep $COMMENT)" ]]
-    do
-        $SUDO iptables -t nat -L POSTROUTING --line-numbers | grep $COMMENT | awk '{print $1}' | xargs $SUDO iptables -t nat -D POSTROUTING
+	$SUDO iptables -t nat -S | grep $COMMENT | awk -v SUDO="$SUDO" '{$1="";$COMMEND=SUDO" iptables -t nat -D "$0; system($COMMEND)}'
     done
 }
 
@@ -58,6 +58,8 @@ if [ "$1" == "forward" ]; then
     forward
 elif [ "$1" == "monitor" ]; then
     monitor
+elif [ "$1" == "list" ]; then
+    list
 elif [ "$1" == "delete" ]; then
     delete
 else
